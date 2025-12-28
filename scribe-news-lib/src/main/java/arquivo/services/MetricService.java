881858a6ec0bba44f6abaa67e03332bd6bbfc123
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-12-28 23:32:35 +0000

    fix(scribe-lib): improves metric first load

--- scribe-news-lib/src/main/java/arquivo/services/MetricService.java
+++ scribe-news-lib/src/main/java/arquivo/services/MetricService.java
@@ -18,6 +18,7 @@ public class MetricService {
     public synchronized void updateValue(String key, long value) {
         final Metric metric = metricRepository.findByKey(key);
         if (metric == null) {
+            // should not happen normally, but just in case
             metricRepository.save(new Metric(key, value));
         } else {
             final long updatedValue = metric.getValue() + value;
@@ -26,12 +27,14 @@ public class MetricService {
         }
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public long loadValue(String key) {
         Metric metric = metricRepository.findByKey(key);
         if (metric != null) {
             return metric.getValue();
+        }else{
+            metric = metricRepository.save(new Metric(key, 0));
         }
-        return 0;
+        return metric.getValue();
     }
 }
