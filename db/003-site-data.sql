5911730b744e7c65c7eb2ad77e6222635bde1d32
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2026-02-09 19:02:28 +0000

    chore(db): skips observador (temp)

--- db/003-site-data.sql
+++ db/003-site-data.sql
@@ -2,5 +2,5 @@ insert into site(name, acronym, url) values
     ('Público', null, 'http://www.publico.pt'),
     ('Diário de Notícias', 'DN', 'http://www.dn.pt'),
     ('Jornal de Notícias', 'JN', 'http://www.jn.pt'),
-    ('Expresso', null, 'http://www.expresso.pt'),
-    ('Observador',null,'https://observador.pt');
+    ('Expresso', null, 'http://www.expresso.pt');
+    --('Observador',null,'https://observador.pt');
