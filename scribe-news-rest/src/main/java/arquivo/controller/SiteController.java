edf6bf4e6efa99c4c69078287f480eb723c98fde
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2026-02-18 21:06:21 +0000

    feat(news-rest): adds site controller

---
+++ scribe-news-rest/src/main/java/arquivo/controller/SiteController.java
@@ -0,0 +1,26 @@
+package arquivo.controller;
+
+import arquivo.model.Site;
+import arquivo.repository.SiteRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sites")
+public class SiteController {
+
+    private final SiteRepository siteRepository;
+
+    public SiteController(SiteRepository siteRepository) {
+        this.siteRepository = siteRepository;
+    }
+
+    @GetMapping()
+    public List<Site> getSites() {
+        return siteRepository.findAll();
+    }
+
+}
