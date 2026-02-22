b057861be0f2eb3506bb6da34efb18535106c7c8
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2026-02-22 08:10:30 +0000

    chore(news-rest): simplifies rest controller code

--- scribe-news-rest/src/main/java/arquivo/controller/CollectionController.java
+++ scribe-news-rest/src/main/java/arquivo/controller/CollectionController.java
@@ -6,9 +6,7 @@ import arquivo.repository.CollectionRepository;
 import arquivo.services.CollectionService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
@@ -26,57 +24,53 @@ public class CollectionController {
     }
 
     @GetMapping("/public")
-    public ResponseEntity<List<CollectionRepository.CollectionPreview>> getPublicCollections() {
-        List<CollectionRepository.CollectionPreview> collections = collectionService.getPublicCollections();
-        return ResponseEntity.ok(collections);
+    public List<CollectionRepository.CollectionPreview> getPublicCollections() {
+        return collectionService.getPublicCollections();
     }
 
     @GetMapping("/public/{collectionId}/articles")
-    public Page<Article> getColletion(@PathVariable int collectionId,
-                                      @RequestParam(defaultValue = "0") int page,
-                                      @RequestParam(defaultValue = "20") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return collectionService.getArticlesBytCollectionId(collectionId, null, pageable);
+    public Page<Article> getPublicCollection(@PathVariable int collectionId,
+                                             @RequestParam(defaultValue = "0") int page,
+                                             @RequestParam(defaultValue = "20") int size) {
+        return collectionService.getArticlesBytCollectionId(collectionId, null, PageRequest.of(page, size));
     }
 
     @GetMapping("/{collectionId}/articles")
-    public Page<Article> getColletion(@AuthenticationPrincipal OAuth2User user,
-                                      @PathVariable int collectionId,
-                                      @RequestParam(defaultValue = "0") int page,
-                                      @RequestParam(defaultValue = "20") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return collectionService.getArticlesBytCollectionId(collectionId, user.getAttribute("sub"), pageable);
+    public Page<Article> getCollection(@AuthenticationPrincipal OAuth2User user,
+                                       @PathVariable int collectionId,
+                                       @RequestParam(defaultValue = "0") int page,
+                                       @RequestParam(defaultValue = "20") int size) {
+        return collectionService.getArticlesBytCollectionId(collectionId, user.getAttribute("sub"), PageRequest.of(page, size));
     }
 
-
     @PostMapping
-    public ResponseEntity<Collection> createCollection(@AuthenticationPrincipal OAuth2User user, @RequestParam String name) {
-        Collection collection = collectionService.createCollection(user.getAttribute("sub"), name);
-        return ResponseEntity.status(HttpStatus.CREATED).body(collection);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Collection createCollection(@AuthenticationPrincipal OAuth2User user, @RequestParam String name) {
+        return collectionService.createCollection(user.getAttribute("sub"), name);
     }
 
     @DeleteMapping("/{collectionId}")
-    public ResponseEntity<Void> deleteCollection(@AuthenticationPrincipal OAuth2User user, @PathVariable int collectionId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCollection(@AuthenticationPrincipal OAuth2User user, @PathVariable int collectionId) {
         collectionService.deleteCollection(user.getAttribute("sub"), collectionId);
-        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<CollectionRepository.CollectionPreview>> getCollections(@AuthenticationPrincipal OAuth2User user) {
-        List<CollectionRepository.CollectionPreview> collections = collectionService.getCollectionsByUser(user.getAttribute("sub"));
-        return ResponseEntity.ok(collections);
+    public List<CollectionRepository.CollectionPreview> getCollections(@AuthenticationPrincipal OAuth2User user) {
+        return collectionService.getCollectionsByUser(user.getAttribute("sub"));
     }
 
-
     @PostMapping("/{collectionId}/articles/{articleId}")
-    public ResponseEntity<Collection> addArticleToCollection(@AuthenticationPrincipal OAuth2User user, @PathVariable int collectionId, @PathVariable int articleId) {
-        Collection collection = collectionService.addArticleToCollection(user.getAttribute("sub"), collectionId, articleId);
-        return ResponseEntity.ok(collection);
+    public Collection addArticleToCollection(@AuthenticationPrincipal OAuth2User user,
+                                             @PathVariable int collectionId,
+                                             @PathVariable int articleId) {
+        return collectionService.addArticleToCollection(user.getAttribute("sub"), collectionId, articleId);
     }
 
     @DeleteMapping("/{collectionId}/articles/{articleId}")
-    public ResponseEntity<Collection> removeArticleFromCollection(@AuthenticationPrincipal OAuth2User user, @PathVariable int collectionId, @PathVariable int articleId) {
-        Collection collection = collectionService.removeArticleFromCollection(user.getAttribute("sub"), collectionId, articleId);
-        return ResponseEntity.ok(collection);
+    public Collection removeArticleFromCollection(@AuthenticationPrincipal OAuth2User user,
+                                                  @PathVariable int collectionId,
+                                                  @PathVariable int articleId) {
+        return collectionService.removeArticleFromCollection(user.getAttribute("sub"), collectionId, articleId);
     }
 }
