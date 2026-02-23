3a5e98886ec591031a01dc16c74756244016c6fa
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2026-02-23 18:54:25 +0000

    fix(news-rest): fixes imports

--- scribe-news-rest/src/main/java/arquivo/controller/GoogleAuthController.java
+++ scribe-news-rest/src/main/java/arquivo/controller/GoogleAuthController.java
@@ -6,12 +6,8 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collections;
 import java.util.Map;
