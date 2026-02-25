e17a4c5764285a5880f9b8c12a110eecbf551658
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2026-02-25 20:59:14 +0000

    feat(db): adds party leaders to insert data

---
+++ db/007-party-leaders.sql
@@ -0,0 +1,49 @@
+insert into person (name) values ('Mário Soares');
+insert into person (name) values ('Vítor Constâncio');
+insert into person (name) values ('Jorge Sampaio');
+insert into person (name) values ('António Guterres');
+insert into person (name) values ('Eduardo Ferro Rodrigues');
+insert into person (name) values ('José Sócrates');
+insert into person (name) values ('António José Seguro');
+insert into person (name) values ('António Costa');
+insert into person (name) values ('Pedro Nuno Santos');
+
+insert into person (name) values ('Francisco Sá Carneiro');
+insert into person (name) values ('Francisco Pinto Balsemão');
+insert into person (name) values ('Aníbal Cavaco Silva');
+insert into person (name) values ('Eurico de Melo');
+insert into person (name) values ('Fernando Nogueira');
+insert into person (name) values ('Marcelo Rebelo de Sousa');
+insert into person (name) values ('Durão Barroso');
+insert into person (name) values ('Pedro Santana Lopes');
+insert into person (name) values ('Luís Marques Mendes');
+insert into person (name) values ('Manuela Ferreira Leite');
+insert into person (name) values ('Pedro Passos Coelho');
+insert into person (name) values ('Rui Rio');
+insert into person (name) values ('Luís Montenegro');
+
+insert into person (name) values ('Álvaro Cunhal');
+insert into person (name) values ('Carlos Carvalhas');
+insert into person (name) values ('Jerónimo de Sousa');
+insert into person (name) values ('Paulo Raimundo');
+
+insert into person (name) values ('Adelino Amaro da Costa');
+insert into person (name) values ('Freitas do Amaral');
+insert into person (name) values ('Basílio Horta');
+insert into person (name) values ('Manuel Monteiro');
+insert into person (name) values ('Paulo Portas');
+insert into person (name) values ('Assunção Cristas');
+insert into person (name) values ('Francisco Rodrigues dos Santos');
+
+insert into person (name) values ('Francisco Louçã');
+insert into person (name) values ('Luís Fazenda');
+insert into person (name) values ('Fernando Rosas');
+insert into person (name) values ('Catarina Martins');
+insert into person (name) values ('Mariana Mortágua');
+
+insert into person (name) values ('André Ventura');
+
+insert into person (name) values ('Rui Tavares');
+
+insert into person (name) values ('João Cotrim de Figueiredo');
+insert into person (name) values ('Rui Rocha');
