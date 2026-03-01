e74e07d6fe25d45a655e5744a24eec9836f074d5
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2026-03-01 09:39:21 +0000

    fix(db): fixes person names

--- db/007-party-leaders.sql
+++ db/007-party-leaders.sql
@@ -1,35 +1,35 @@
-insert into person (name) values ('Mário Soares');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Mário Soares');
 insert into person (name) values ('Vítor Constâncio');
-insert into person (name) values ('Jorge Sampaio');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Jorge Sampaio');
 insert into person (name) values ('António Guterres');
 insert into person (name) values ('Eduardo Ferro Rodrigues');
 insert into person (name) values ('José Sócrates');
 insert into person (name) values ('António José Seguro');
-insert into person (name) values ('António Costa');
+-- already in 005-ministers.sql: insert into person (name) values ('António Costa');
 insert into person (name) values ('Pedro Nuno Santos');
 
 insert into person (name) values ('Francisco Sá Carneiro');
 insert into person (name) values ('Francisco Pinto Balsemão');
-insert into person (name) values ('Aníbal Cavaco Silva');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Aníbal Cavaco Silva');
 insert into person (name) values ('Eurico de Melo');
 insert into person (name) values ('Fernando Nogueira');
-insert into person (name) values ('Marcelo Rebelo de Sousa');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Marcelo Rebelo de Sousa');
 insert into person (name) values ('Durão Barroso');
 insert into person (name) values ('Pedro Santana Lopes');
-insert into person (name) values ('Luís Marques Mendes');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Luís Marques Mendes');
 insert into person (name) values ('Manuela Ferreira Leite');
 insert into person (name) values ('Pedro Passos Coelho');
 insert into person (name) values ('Rui Rio');
 insert into person (name) values ('Luís Montenegro');
 
 insert into person (name) values ('Álvaro Cunhal');
-insert into person (name) values ('Carlos Carvalhas');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Carlos Carvalhas');
 insert into person (name) values ('Jerónimo de Sousa');
 insert into person (name) values ('Paulo Raimundo');
 
 insert into person (name) values ('Adelino Amaro da Costa');
 insert into person (name) values ('Freitas do Amaral');
-insert into person (name) values ('Basílio Horta');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Basílio Horta');
 insert into person (name) values ('Manuel Monteiro');
 insert into person (name) values ('Paulo Portas');
 insert into person (name) values ('Assunção Cristas');
@@ -37,13 +37,13 @@ insert into person (name) values ('Francisco Rodrigues dos Santos');
 
 insert into person (name) values ('Francisco Louçã');
 insert into person (name) values ('Luís Fazenda');
-insert into person (name) values ('Fernando Rosas');
-insert into person (name) values ('Catarina Martins');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Fernando Rosas');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('Catarina Martins');
 insert into person (name) values ('Mariana Mortágua');
 
-insert into person (name) values ('André Ventura');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('André Ventura');
 
 insert into person (name) values ('Rui Tavares');
 
-insert into person (name) values ('João Cotrim de Figueiredo');
+-- already in 006-president-candidates-data.sql: insert into person (name) values ('João Cotrim de Figueiredo');
 insert into person (name) values ('Rui Rocha');
