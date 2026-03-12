75cda950332499d4fa734a999e6bdce3a9833cd8
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2026-03-12 19:08:24 +0000

    fix(news-lib): fixes authors paginated query to return only authors with more than 5 articles

--- scribe-news-lib/src/main/java/arquivo/repository/AuthorRepository.java
+++ scribe-news-lib/src/main/java/arquivo/repository/AuthorRepository.java
@@ -4,6 +4,7 @@ import arquivo.model.Author;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,5 +13,15 @@ public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
     Optional<Author> findByName(String name);
 
+    @Query(value = """
+        select a
+        from Author a
+        where (select count(ar) from Article ar where ar.author = a) > 5
+        """,
+            countQuery = """
+        select count(a)
+        from Author a
+        where (select count(ar) from Article ar where ar.author = a) > 5
+        """)
     Page<Author> findAll(Pageable pageable);
 }
